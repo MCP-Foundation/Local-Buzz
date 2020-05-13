d28e5426fc7f93cf15743e8b5cd9d519d93059e9
Carmen Salas <carmensalas@Carmens-MacBook-Pro.local>
2020-05-13 15:50:30 -0400

    created banner component with header andforum button

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Navigation from '../Navigation/Navigation';
+import Banner from '../HomePage/Banner';
 
 class HomePage extends Component {
   render() {
     return (
       <section className="HomePage">
         <Navigation />
+        <Banner />
       </section>
     );
   }
