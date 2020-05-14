5f023029147ae5621963790e11cde94296aeb262
Carmen Salas <carmensalas@Carmens-MacBook-Pro.local>
2020-05-14 13:57:07 -0400

    changed Navigation component to use navbar from react bootstrap

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,31 +1,26 @@
 import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+
 import '../Navigation/Navigation.css';
 
 import Logo from '../Logo';
 
 function Navigation() {
   return (
-    <section className="NavigationComponent">
-      <nav>
-        <Logo />
-        <ol>
-          <li className="navTab" className="navTab">
-            <a href="/">Home</a>{' '}
-          </li>
-          <li className="navTab">
-            <a href="/">Forum</a>{' '}
-          </li>
-          <li className="navTab">
-            <a href="/">Account</a>{' '}
-          </li>
-          <li className="navTab">
-            <a href="/">Sign In</a>{' '}
-          </li>
-          <li className="navTab">
-            <a href="/">Sign Up</a>{' '}
-          </li>
-        </ol>
-      </nav>
+    <section className="NavbarComponent">
+      <Navbar bg="light" expand="lg" fixed="top">
+        <Navbar.Brand href="/">
+          <Logo />
+        </Navbar.Brand>
+        <Nav className="mr-auto">
+          <Nav.Link href="/">Home</Nav.Link>
+          <Nav.Link href="/posts">Posts</Nav.Link>
+          <Nav.Link href="/account">Account</Nav.Link>
+          <Nav.Link href="/login">Sign In</Nav.Link>
+          <Nav.Link href="/register">Sign Up</Nav.Link>
+        </Nav>
+      </Navbar>
     </section>
   );
 }
