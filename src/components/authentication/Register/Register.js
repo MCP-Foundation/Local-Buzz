d350a5afc2d1304053ed998773532b06f7ba87d5
Carmen Salas <carmensalas0114@gmail.com>
2020-05-15 03:16:18 -0400

    created Login component with Login form and Register component with Register form, added css file for authentication

---
+++ src/components/authentication/Register/Register.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import '../../authentication/authentication.css';
+import Navigation from '../../Navigation/Navigation';
+import RegisterForm from './RegisterForm';
+
+class Register extends Component {
+  render() {
+    return (
+      <section className="RegisterComponent">
+        <Navigation />
+        <h1>Sign up to create an account</h1>
+        <RegisterForm />
+      </section>
+    );
+  }
+}
+
+export default Register;
