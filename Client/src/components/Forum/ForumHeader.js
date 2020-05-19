6ebc3f489f2f4e366e2cc8f12f17a779607c441c
Carmen Salas <carmensalas0114@gmail.com>
2020-05-19 12:57:06 -0400

    git stash pop merged with master

--- Client/src/components/Forum/ForumHeader.js
+++ Client/src/components/Forum/ForumHeader.js
@@ -7,7 +7,11 @@ function ForumHeader({ postFormRedirect }) {
         <h3 className="forumHeader">
           Checkout different tags and topics people are talking about
         </h3>
+<<<<<<< Updated upstream:Client/src/components/Forum/ForumHeader.js
         <button onClick={postFormRedirect}>Make a Post</button>
+=======
+        <button>Make a Post</button>
+>>>>>>> Stashed changes:src/components/Forum/ForumHeader.js
       </header>
     </section>
   );
