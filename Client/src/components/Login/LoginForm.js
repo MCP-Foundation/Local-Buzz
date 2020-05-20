fbaec11616f379bac2576ecb4b6ebc56e10d0ffc
Peter Rose <52871421+PeterTheDeveloper@users.noreply.github.com>
2020-05-20 08:03:22 -0400

    Register form styled

--- Client/src/components/Login/LoginForm.js
+++ Client/src/components/Login/LoginForm.js
@@ -46,7 +46,7 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function LoginForm(loginUser) {
+export default function LoginForm({ loginUser }) {
   const classes = useStyles();
 
   return (
@@ -119,23 +119,3 @@ export default function LoginForm(loginUser) {
     </Container>
   );
 }
-
-// function LoginForm({ loginUser }) {
-//   return (
-//     <section className="LoginFormComponent">
-//       <form onSubmit={loginUser} id="loginForm" action="/api/login" method="post">
-//         <fieldset>
-//           <label htmlFor="email">Email</label>
-//           <input id="email" type="email" name="email" />
-
-//           <label htmlFor="password">Password</label>
-//           <input id="password" type="password" name="password" />
-
-//           <input type="submit" value="Log In" />
-//         </fieldset>
-//       </form>
-//     </section>
-//   );
-// }
-
-// export default LoginForm;
