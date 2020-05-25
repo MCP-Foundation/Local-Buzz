245edffaab096605228286f5299b2734c4d719da
Deteriator <markgriffith009@gmail.com>
2020-05-25 14:51:53 -0400

    finalized function for logout.

--- Client/src/components/Navigation/Navigation.jsx
+++ Client/src/components/Navigation/Navigation.jsx
@@ -3,17 +3,13 @@ import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import '../Navigation/Navigation.css';
 import Logo from '../Logo';
-import UserProvider  from '../../contexts/userContext'
 
 function Navigation() {
-
-  const isLoggedIn  = useContext(UserProvider);
-  console.log(isLoggedIn)
 
   function deleteCookie(name) {
     document.cookie = name + '=;expires=Thu, 01 Jan 1970 00:00:01 GMT;';
   }
-  function whatever(){
+  function logoutUser(){
     deleteCookie('userToken')
   }
   return (
@@ -26,7 +22,7 @@ function Navigation() {
           <Nav.Link href="/">About</Nav.Link>
           <Nav.Link href="/forum">Forum</Nav.Link>
           {document.cookie ? <Nav.Link href="/account">Account</Nav.Link>: <p></p> }
-          {document.cookie ? <Nav.Link onClick={whatever} href="/logout">Log Out</Nav.Link> : <Nav.Link href="/login">Sign In</Nav.Link>}
+          {document.cookie ? <Nav.Link onClick={logoutUser} href="/logout">Log Out</Nav.Link> : <Nav.Link href="/login">Sign In</Nav.Link>}
           {document.cookie ? <p></p> : <Nav.Link href="/register">Sign Up</Nav.Link> }
         </Nav>
       </Navbar>
