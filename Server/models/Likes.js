4b698aae27a666959aef8edbe16942ad11f8c826
Carmen Salas <carmensalas0114@gmail.com>
2020-05-28 11:57:50 -0400

    returned response rows in models

--- Server/models/Likes.js
+++ Server/models/Likes.js
@@ -1,20 +1,19 @@
-const db = require('../db')
+const db = require('../db');
 
 class Likes {
-  static addALike (userId, postId) {
-    const queryText = 'INSERT INTO likes (user_id, post_id) VALUES ($1, $2);'
-    return db.query(queryText, [userId, postId])
+  static add(userId, postId) {
+    const queryText = 'INSERT INTO likes (user_id, post_id) VALUES ($1, $2);';
+    return db.query(queryText, [userId, postId]).then((res) => res.rows);
   }
 
-  static deleteALike (userId, postId) {
-    const queryText = 'DELETE FROM likes WHERE user_id = $1 AND post_id = $2;'
-    return db.query(queryText, [userId, postId])
+  static remove(userId, postId) {
+    const queryText = 'DELETE FROM likes WHERE user_id = $1 AND post_id = $2;';
+    return db.query(queryText, [userId, postId]).then((res) => res.rows);
   }
 
-  static amountOfLike (postId) {
-    const queryText = 'SELECT * FROM likes WHERE post_id=$1;'
-    return db.query(queryText, [postId])
-      .then((response) => response.rows)
+  static getByPostId(postId) {
+    const queryText = 'SELECT * FROM likes WHERE post_id=$1;';
+    return db.query(queryText, [postId]).then((res) => res.rows);
   }
 }
-module.exports = Likes
+module.exports = Likes;
