7c427340ee9733c8c810dfc565c2d51017349ea1
Deteriator <markgriffith009@gmail.com>
2020-05-28 12:26:57 -0400

    logout reroutes to about page.

--- Client/src/components/Navigation/Navigation.jsx
+++ Client/src/components/Navigation/Navigation.jsx
@@ -5,15 +5,6 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 import '../Navigation/Navigation.css';
 import Logo from '../Logo';
 
-// import {NavDropdown} from '@react-bootstrap.react-bootstrap.nav-dropdown'
-// import {Nav} from '@react-bootstrap.react-bootstrap.nav'
-// import { Button } from '@react-bootstrap.react-bootstrap.form'
-// import {Form} from '@react-bootstrap.react-bootstrap.form'
-// import {FormControl} from '@react-bootstrap.react-bootstrap.form-control'
-// import {Navbar} from '@react-bootstrap.react-bootstrap.navbar'
-// import ReactBootstrapStyle from '@react-bootstrap.react-bootstrap.internal.style-links';
-
-
 function Navigation() {
 
   function deleteCookie(name) {
@@ -37,7 +28,7 @@ function Navigation() {
             <Nav.Link href="/">About</Nav.Link>
             {document.cookie ? <Nav.Link href="/forum">Forum</Nav.Link>: <p></p>}
             {document.cookie ? <Nav.Link className='nav-item' href="/account">Account</Nav.Link> : <p></p>}
-            {document.cookie ? <Nav.Link className='nav-item' onClick={logoutUser} href="/logout">Log Out</Nav.Link> : <Nav.Link className='nav-item' href="/login">Sign In</Nav.Link>}
+            {document.cookie ? <Nav.Link className='nav-item' onClick={logoutUser} href="/">Log Out</Nav.Link> : <Nav.Link className='nav-item' href="/login">Sign In</Nav.Link>}
             {document.cookie ? <p></p> : <Nav.Link className='nav-item signup' href="/register">Sign Up</Nav.Link>}
           </Nav>
         </Navbar.Collapse>
