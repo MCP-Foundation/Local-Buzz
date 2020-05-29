8d1ed8a8a7db11e9b0aad6b07cb056716b74578d
Carmen Salas <carmensalas0114@gmail.com>
2020-05-28 22:30:13 -0400

    Styled homepaged added footer and tag icons

--- Client/src/components/HomePage/About.js
+++ Client/src/components/HomePage/About.js
@@ -6,10 +6,10 @@ function About() {
     <section className="AboutComponent">
       <article>
         <Paragraph size="large" className="homepageArticle">
-          Small Stories is a small business forum for small business owners and
-          employees to share advice and stories to empower eachother. Explore
+          Local Buzz is a small business forum for small business owners and
+          employees to share advice and stories to empower each other. Explore
           these tags that small business owners and employees are using to
-          connect with eachother!
+          connect!
         </Paragraph>
       </article>
     </section>
