5f3544ed67aa6c064905553dbb4fb7a4fff2d7ac
Carmen Salas <carmensalas0114@gmail.com>
2020-05-29 03:35:35 -0400

    styled login and register forms to have grommet buttons

--- Client/src/components/HomePage/Tag.js
+++ Client/src/components/HomePage/Tag.js
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Button, Paragraph } from 'grommet';
+import { TagContext } from '../../contexts/tagContext';
+import { Link } from 'react-router-dom';
 
 function Tag({ tagName, tagDescription, tagIcon }) {
+  const [tag, setTag] = useContext(TagContext);
+
   return (
     <section className="tagComponent">
       <div>
-        <img className="tagIcon" src={tagIcon} />
+        <img className="tagIcon" src={tagIcon}></img>
       </div>
-      <div>
-        {' '}
-        <Button primary label={`#${tagName}`} />
+      <div className="tagButton">
+        <Link to="/forum" replace>
+          <Button
+            primary
+            label={`#${tagName}`}
+            onClick={() => setTag(`#${tagName}`)}
+          />
+        </Link>
       </div>
 
       <section className="TagDescription">
